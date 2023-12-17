af1f65aab1cf760b40275a265577286853d24772
Your Name <you@example.com>
2023-12-16 20:45:00 -0800

    Start

--- code/sprite.cpp
+++ code/sprite.cpp
@@ -11,7 +11,6 @@ int main()
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 
-	// Create and open a window for the game
 	RenderWindow window(vm, "Flower Fields", Style::Default);
 
 	// Create a texture to hold a graphic on the GPU
@@ -19,25 +18,49 @@ int main()
 
 	// Load a graphic into the texture
 	textureBackground.loadFromFile("mondt_theme.png");
+	cout << "Texture size: "<< textureBackground.getSize().x << " x " << textureBackground.getSize().y << " y " << endl;
 
 	// Create a sprite
 	Sprite spriteBackground;
-
-	// Attach the texture to the sprite
 	spriteBackground.setTexture(textureBackground);
-
-	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(0, 0);
 
 
-	while (window.isOpen())
-	{
+    View view(FloatRect(0, 0, static_cast<float>(textureBackground.getSize().x), static_cast<float>(textureBackground.getSize().y)));
+    window.setView(view);
 
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
+
+	while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+            else if (event.type == Event::Resized)
+            {
+                // Adjust the view to maintain the correct aspect ratio
+                float aspectRatioWindow = static_cast<float>(event.size.width) / static_cast<float>(event.size.height);
+                float aspectRatioImage = static_cast<float>(textureBackground.getSize().x) / static_cast<float>(textureBackground.getSize().y);
+
+                if (aspectRatioWindow > aspectRatioImage)
+                {
+                    // Window is wider than the image, adjust height
+                    float newHeight = static_cast<float>(event.size.width) / aspectRatioImage;
+                    View view(FloatRect(0, 0, static_cast<float>(event.size.width), newHeight));
+                    window.setView(view);
+                }
+                else
+                {
+                    // Window is taller than the image, adjust width
+                    float newWidth = static_cast<float>(event.size.height) * aspectRatioImage;
+                    View view(FloatRect(0, 0, newWidth, static_cast<float>(event.size.height)));
+                    window.setView(view);
+                }
+            }
+        }
 
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
